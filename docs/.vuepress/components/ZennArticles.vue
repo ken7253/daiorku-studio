f5830e14331b92046c730f9e9e57edef56a9612c
ken7253 <ken7253@gmail.com>
2021-01-07 01:15:35 +0900

    feat: add api request func

--- docs/.vuepress/components/ZennArticles.vue
+++ docs/.vuepress/components/ZennArticles.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <h3>Zennの記事</h3>
+    <ul>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
+export default {
+  data: function(){
+    return {
+
+    }
+  },
+  methods: {
+    getArticles() {
+      const requestUrl = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fzenn.dev%2Fken7253%2Ffeed"
+      axios.get(requestUrl)
+        .then(resp => {console.log(resp)})
+        .catch(err => {
+          console.error(err)
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>

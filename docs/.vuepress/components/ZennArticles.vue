38d6bfb1e5705666b188aaa577c6c49bac0590ba
ken7253 <ken7253@gmail.com>
2021-01-08 01:46:45 +0900

    feat: display zenn articles

--- docs/.vuepress/components/ZennArticles.vue
+++ docs/.vuepress/components/ZennArticles.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
+  <div class="zenn-articles">
     <h3>Zennの記事</h3>
-    <ul>
-      <li></li>
+    <div v-if="error.flag">
+      <p>記事一覧の取得に失敗しました。</p>
+      <code>{{ error.log }}</code>
+    </div>
+    <ul v-if="!error.flag">
+      <li v-for="(item, index) in items" :key="index">
+        <a :href="item.link" target="_blank" rel="noopener noreferrer">
+          <img :src="item.enclosure.link" alt="item.title" />
+          <time>{{ item.pubDate }}</time>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
@@ -11,22 +20,50 @@
 import axios from "axios";
 
 export default {
-  data: function(){
+  data: function() {
     return {
-
-    }
+      items: null,
+      error: {
+        flag: false,
+        log: ""
+      }
+    };
   },
-  methods: {
-    getArticles() {
-      const requestUrl = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fzenn.dev%2Fken7253%2Ffeed"
-      axios.get(requestUrl)
-        .then(resp => {console.log(resp)})
-        .catch(err => {
-          console.error(err)
+  created: function() {
+    const base = "https://api.rss2json.com/v1/api.json";
+    const request = "?rss_url=https%3A%2F%2Fzenn.dev%2Fken7253%2Ffeed";
+    axios
+      .get(base + request)
+      .then((resp) => {
+        this.items = resp.data.items;
       })
-    }
-  }
-}
+      .catch((err) => {
+        console.warn(err);
+        this.error.log = err;
+        this.error.flag = true;
+      });
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.zenn-articles ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.zenn-articles ul li {
+  width: 270px;
+}
+.zenn-articles ul li a {
+  display: block;
+}
+.zenn-articles ul time {
+  display: block;
+  text-align: end;
+  font-size: 0.75rem;
+}
+</style>

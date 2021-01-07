b319d32109f7b871188fe556d256b10237b9c9b1
ken7253 <ken7253@gmail.com>
2021-01-08 02:00:48 +0900

    feat: enable nav menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,12 @@ module.exports = {
   description:
     "dairoku-studio blogという名の個人ブログそこまで技術的な話じゃないものはこちらで",
   themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/blog'},
+      { text: 'Github', link: 'https://github.com/ken7253' },
+      { text: 'Twitter', link: 'https://twitter.com/ken7253_'},
+    ],
     lastUpdated: 'Last Updated',
     smoothScroll: true,
   },

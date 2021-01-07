9db9f4805fd2c0e1b2ee0bbec4e60c11839b72cb
ken7253 <ken7253@gmail.com>
2021-01-08 04:08:46 +0900

    fix: site title and description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,9 @@ module.exports = {
       lang: "ja",
     },
   },
-  title: "dairoku-studio blog",
+  title: "dairoku-studio",
   description:
-    "dairoku-studio blogという名の個人ブログそこまで技術的な話じゃないものはこちらで",
+    "dairoku-studioという名の個人ブログ。そこまで技術的な話じゃないものはこちらで",
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },

2346c0119fcf15e8169c99e60e7bad81e2d6230e
ken7253 <ken7253@gmail.com>
2021-01-09 03:05:19 +0900

    style: display publish date and style fmt

--- docs/.vuepress/components/BlogList.vue
+++ docs/.vuepress/components/BlogList.vue
@@ -2,10 +2,13 @@
   <div>
     <p v-if="!posts">まだ投稿がありません。</p>
     <ul v-if="posts">
-      <li v-for="(post,index) in posts">
+      <li v-for="(post, index) in posts">
         <a :href="post.path">
           <p>{{ post.title }}</p>
-          <p v-if="post.lastUpdated"><time>{{ post.lastUpdated }}</time></p>
+          <p class="date">
+            Date: <time v-if="post.frontmatter.date">{{ post.frontmatter.date | dateFmt() }}</time>
+            Update: <time v-if="post.lastUpdated">{{ post.lastUpdated | updateFmt() }}</time>
+          </p>
         </a>
       </li>
     </ul>
@@ -22,6 +25,28 @@ export default {
   created: function() {
     this.posts = this.$site.pages.filter((page) => page.id === "post");
   },
+  filters: {
+    dateFmt: function(value) {
+      if (!value) {
+        return "";
+      } else {
+        const postDate = value
+          .split("T")
+          .shift()
+          .split("-")
+          .join("/");
+        return postDate;
+      }
+    },
+    updateFmt: function (value) {
+      if (!value) {
+        return "";
+      }else {
+        const update = value.split(" ").shift()
+        return update;
+      }
+    }
+  },
 };
 </script>
 <style scoped>
@@ -29,7 +54,7 @@ li p {
   padding: 0;
   margin: 0;
 }
-time {
-  font-size: .75rem;
+.date {
+  font-size: 0.75rem;
 }
 </style>

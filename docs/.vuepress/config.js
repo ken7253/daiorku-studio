9ff3637847af8d7c274bf3fdb8c5ea904ed1c1ee
ken7253 <ken7253@gmail.com>
2021-01-08 20:54:34 +0900

    feat: enable ogp

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,8 @@
 module.exports = {
   head: [
-    [
-      "meta",
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-    ],
+    ["meta",{ charset: "utf-8" }],
+    ["meta",{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+    ["meta",{ name: "og:image", content: "./kenshiro.png"}]
   ],
   title: "dairoku-studio",
   description:
@@ -15,6 +13,7 @@ module.exports = {
     },
   },
   themeConfig: {
+    domain: "https://dairoku-studio.com",
     nav: [
       { text: "Home", link: "/" },
       { text: "Works", link: "/works" },
@@ -22,24 +21,25 @@ module.exports = {
       { text: "Github", link: "https://github.com/ken7253" },
       { text: "Twitter", link: "https://twitter.com/ken7253_" },
     ],
+    author: "ken7253",
     displayAllHeaders: true,
     lastUpdated: "Last Updated",
     smoothScroll: true,
   },
-  plugins: [
-    [
-      "@vuepress/blog",
-      {
-        directories: [
-          {
-            id: "post",
-            dirname: "_posts",
-            path: "/post/",
-            itemPermalink: "/post/:year/:month/:day/:slug",
-          },
-        ],
-      },
-    ],
-  ],
+  plugins: {
+    "@vuepress/blog": {
+      directories: [
+        {
+          id: "post",
+          dirname: "_posts",
+          path: "/post/",
+          itemPermalink: "/post/:year/:month/:day/:slug",
+        },
+      ],
+    },
+    seo: {
+      twitterCard: _ => 'summary',
+    },
+  },
   dest: "./dist",
 };

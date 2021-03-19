c15fbebbed5beb87b4fc38e747cb37fdb1c8e538
ken7253 <ken7253@gmail.com>
2021-03-20 02:43:50 +0900

    fix v-bind key

--- docs/.vuepress/components/BlogList.vue
+++ docs/.vuepress/components/BlogList.vue
@@ -2,7 +2,7 @@
   <div>
     <p v-if="!posts">まだ投稿がありません。</p>
     <ul v-if="posts">
-      <li v-for="(post, index) in posts" :key="index">
+      <li v-for="post in posts" :key="post.key" :class="post.key">
         <a :href="post.path">
           <h2>{{ post.title }}<br><span>{{post.frontmatter.description}}</span></h2>
           <p class="date">

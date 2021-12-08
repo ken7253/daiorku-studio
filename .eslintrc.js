aa2c275d0dec2a7129873d4474803ebc638b3732
ken7253 <ken7253@gmail.com>
2021-12-08 23:07:15 +0900

    chore: enable eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: [],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+  ],
+  rules: {}
+}
